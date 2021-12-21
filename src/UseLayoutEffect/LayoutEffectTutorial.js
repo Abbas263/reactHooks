83c75b69d7c9bf00b9322b0403ff7e7c1e1bdd21
abbas ali <abbas.ali5788@gmail.com>
2021-12-21 10:54:48 +0500

    understand useLayoutEffect by printing value of input in console

--- src/UseLayoutEffect/LayoutEffectTutorial.js
+++ src/UseLayoutEffect/LayoutEffectTutorial.js
@@ -1,15 +1,22 @@
-import React, { useLayoutEffect, useEffect } from "react";
+import React, { useLayoutEffect, useEffect, useRef } from "react";
 
 const LayoutEffectTutorial = () => {
+  const inputRef = useRef(null);
   useLayoutEffect(() => {
+    console.log(inputRef.current.value);
     console.log("useLayoutEffect Called");
   }, []);
   useEffect(() => {
+    inputRef.current.value = "Hello";
     console.log("useEffect Called");
   }, []);
-//   useLayoutEffect is fundamentally called in an earlier stage of page rendering than the useEffect
-// Mean 
-  return <div>Hello World</div>;
+  //   useLayoutEffect is fundamentally called in an earlier stage of page rendering than the useEffect
+  // Mean it is called before rendering the actual content of component/View while useEffect is called after actual content is loaded
+  return (
+    <div className="App">
+      <input ref={inputRef} value="PEDRO" style={{ width: 400, height: 20 }} />
+    </div>
+  );
 };
 
 export default LayoutEffectTutorial;
